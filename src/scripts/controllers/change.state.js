7ea5c937222ec3ce0a4459db3489d7c425cd5d20
Yogesh Choudhary <yogesum@gmail.com>
2015-12-14 20:41:07 +0530

    state change scheduled_on time selector added

--- src/scripts/controllers/change.state.js
+++ src/scripts/controllers/change.state.js
@@ -6,12 +6,24 @@ angular.module('qui')
     'applicant',
     'state_id',
     'ChangeState',
-    function JobsCtrl($uibModalInstance, User, CURRENCY, applicant, stateId, ChangeState) {
+    'moment',
+    function JobsCtrl($uibModalInstance, User, CURRENCY, applicant, stateId, ChangeState, moment) {
       const vm = this;
       vm.states = User.states;
       vm.applicant = applicant;
       vm.stateId = stateId;
       vm.currency = CURRENCY;
+      vm.today = new Date();
+
+      vm.setScheduledOn = function scheduledOnTimeChange() {
+        const hour = moment(vm.exData.scheduled_on_time).get('hour');
+        const minute = moment(vm.exData.scheduled_on_time).get('minute');
+        vm.data.scheduled_on = moment(vm.exData.scheduled_on_date)
+          .set('hour', hour)
+          .set('minute', minute)
+          .set('second', 0)
+          .set('millisecond', 0);
+      };
 
       vm.ok = function ok() {
         ChangeState.set(applicant.id, vm.data)
