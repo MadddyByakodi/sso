8a8be3fbcf727598f7ca2f43048c1c3a26128686
Yogesh Choudhary <yogesum@gmail.com>
2016-03-09 19:01:24 +0530

    required and optional skills integration in jd

--- src/scripts/hire/controllers/applicant.view.js
+++ src/scripts/hire/controllers/applicant.view.js
@@ -1,13 +1,12 @@
 angular.module('qui.hire')
   .controller('ApplicantViewController', [
     'Applicants',
-    'Followers',
     'Page',
     '$stateParams',
     'Session',
     'APP',
     '$sce',
-    function ApplicantViewCtrl(Applicants, Followers, Page, $stateParams, Session, APP, $sce) {
+    function ApplicantViewCtrl(Applicants, Page, $stateParams, Session, APP, $sce) {
       const vm = this;
       vm.data = {};
       vm.trustSrc = function trustSrc(src) {
@@ -22,13 +21,6 @@ angular.module('qui.hire')
           .then(function gotApplicant(result) {
             vm.data = result;
             Page.setTitle(vm.data.name);
-
-            // Loading Followers
-            Followers
-              .getOne($stateParams.applicantId)
-              .then(function gotFollower(fresult) {
-                vm.data.follower = fresult;
-              });
 
             // data has been loaded
             vm.ui.loading = false;
