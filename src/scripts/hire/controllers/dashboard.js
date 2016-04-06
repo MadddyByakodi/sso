e28554688c7cf166005b115d9e234f3d06c91336
Yogesh Choudhary <yogesum@gmail.com>
2016-04-06 15:51:45 +0530

    lazyload for dash board items and logic correction for upcomming interviews

--- src/scripts/hire/controllers/dashboard.js
+++ src/scripts/hire/controllers/dashboard.js
@@ -28,39 +28,65 @@ angular.module('qui.hire')
       };
 
       vm.getSummary();
-
+      vm.pipeParams = { start: 0, rows: 15 };
+      vm.pipeUI = { lazyLoad: true, loading: false };
       vm.getPipeline = function getPipeline() {
-        Summary.getPipeline()
-          .then(function gotPipeline(response) {
-            vm.pipeline = response;
+        vm.pipeUI = { lazyLoad: false, loading: true };
+        Summary.getPipeline(vm.pipeParams)
+          .then(response => {
+            vm.pipeline = (vm.pipeline || [])
+              .concat(!response ? [] : response);
+
+            // data has been loaded
+            vm.pipeUI = {
+              lazyLoad: angular.equals(response.length, vm.pipeParams.rows),
+              loading: false,
+            };
+
+            // increment offset for next loading of results
+            vm.pipeParams.start = vm.pipeParams.start + vm.pipeParams.rows;
           });
       };
 
       vm.getPipeline();
-
+      vm.interviewsParams = {
+        fl: 'id,name,interview_type,interview_time,state_id,_root_',
+        sort: 'interview_time ASC',
+        interview_time: [
+          moment().startOf('day').toISOString(),
+          moment().endOf('week').toISOString(),
+        ].join(','),
+        start: 0, rows: 15,
+      };
+      vm.interviewsUI = { lazyLoad: true, loading: false };
       vm.getInterviews = function getInterviews() {
-        Applicants.get({
-          fl: 'id,name,interview_type,interview_time,state_id,_root_',
-          sort: 'interview_time ASC',
-          interview_time: [
-            moment().startOf('day').toISOString(),
-            moment().endOf('week').toISOString(),
-          ].join(','),
-        }).then(function gotInterviews(response) {
-          vm.interviews = response && response.filter(i => {
-            // hide interview if
-            //   - current state is not an interview
-            //   ---- AND
-            //   - interview time is not in between current time and tommorow
-            const hideInterview =
-              !~[5, 8, 9].indexOf(i.interview_type) &&
-              !(
-                moment(i.interview_time).isAfter(moment()) &&
-                moment(i.interview_time).isBefore(moment().startOf('day').add(2, 'days'))
-              );
-            if (hideInterview) return false;
-            return true;
-          });
+        vm.interviewsUI = { lazyLoad: false, loading: true };
+        Applicants.get(vm.interviewsParams).then(response => {
+          vm.interviews = (vm.interviews || []).concat(
+            !response ? [] : response.filter(i => {
+              // hide interview if
+              //   - current state is not an interview
+              //   ---- AND
+              //   - interview time is not in between current time and tommorow
+              const hideInterview =
+                !~[5, 8, 17].indexOf(i.state_id) &&
+                !(
+                  moment(i.interview_time).isAfter(moment()) &&
+                  moment(i.interview_time).isBefore(moment().startOf('day').add(2, 'days'))
+                );
+              if (hideInterview) return false;
+              return true;
+            })
+          );
+
+          // data has been loaded
+          vm.interviewsUI = {
+            lazyLoad: angular.equals(response.length, vm.interviewsParams.rows),
+            loading: false,
+          };
+
+          // increment offset for next loading of results
+          vm.interviewsParams.start = vm.interviewsParams.start + vm.interviewsParams.rows;
         });
       };
 
