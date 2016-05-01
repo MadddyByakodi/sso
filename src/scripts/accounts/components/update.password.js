a7ce4b0e008cd18a45a223308813599afd2b19dd
Yogesh Choudhary <yogesum@gmail.com>
2016-05-01 21:56:14 +0530

    update password implementation with validations

--- src/scripts/accounts/components/update.password.js
+++ src/scripts/accounts/components/update.password.js
@@ -6,14 +6,20 @@ angular.module('qui.accounts')
         templateUrl: '/html/accounts.update-password.html',
         controllerAs: 'Password',
         controller: [
-          'Password',
-          (Password) => {
+          'APP',
+          '$http',
+          function Password(APP, $http) {
             const vm = this;
-            vm.update = () => Password
-              .update(vm.data)
+            vm.update = '';
+            vm.data = { current: '', update: '' };
+            vm.match = (confirm) => confirm
+              .$setValidity('match', vm.data.update === vm.confirm);
+
+            vm.update = () => (vm.success = vm.error = '') || $http
+              .put(`${APP.apiServer}/user/password`, vm.data)
               .then(() => {
                 vm.success = 'Password update was successful.';
-                vm.data = {};
+                vm.data.current = vm.data.update = vm.confirm = '';
               })
               .catch(res => vm.error = res.data.error_description);
           },
