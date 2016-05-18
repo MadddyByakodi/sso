ca05b8dc45e543dab862f66d858eb5682c289bb4
Yogesh Choudhary <yogesum@gmail.com>
2016-05-18 16:08:40 +0530

    nav jobs search uncollapse closed jobs

--- src/scripts/hire/controllers/nav.jobs.js
+++ src/scripts/hire/controllers/nav.jobs.js
@@ -2,7 +2,9 @@ angular.module('qui.hire')
   .controller('NavJobsController', [
     'Jobs',
     '$state',
-    function NavJobsCtrl(Jobs, $state) {
+    '$scope',
+    '$filter',
+    function NavJobsCtrl(Jobs, $state, $scope, $filter) {
       const vm = this;
       vm.jobHref = function jobHref(jobId) {
         const states = ['app.jobs.manage', 'app.jobs.view', 'app.jobs.edit'];
@@ -11,6 +13,8 @@ angular.module('qui.hire')
       };
 
       vm.jobs = []; // collection of jobs
+      vm.closed = []; // closed jobs
+      vm.open = []; // open jobs
       vm.ui = { lazyLoad: true, loading: false }; // ui states
       vm.params = { start: 0, rows: 15, fl: 'id,role,job_status,owner_id' }; // GET query params
       vm.loadJobs = function loadJobs() {
@@ -24,6 +28,7 @@ angular.module('qui.hire')
             if (!(vm.params.start || result.length)) return $state.go('app.welcome');
             angular.forEach(result, job => {
               vm.jobs.push(job);
+              return (job.job_status === 'Closed' ? vm.closed : vm.open).push(job);
             });
 
             // data has been loaded
@@ -31,14 +36,25 @@ angular.module('qui.hire')
 
             // check for returned results count and set lazy loadLoad false if less
             vm.ui.lazyLoad = angular.equals(result.length, vm.params.rows);
+            if (!vm.ui.lazyLoad) vm.searchText = { role: '' }; // all jobs loaded show in UI
 
             // increment offset for next loading of results
             vm.params.start = vm.params.start + vm.params.rows;
-
             return vm.loadJobs();
           });
       };
 
       vm.loadJobs();
+
+      $scope.$watch(() => vm.searchText, () => {
+        const open = vm.jobs.filter(x => x.job_status !== 'Closed');
+        const closed = vm.jobs.filter(x => x.job_status === 'Closed');
+        vm.open = $filter('filter')(open, vm.searchText);
+        vm.closed = $filter('filter')(closed, vm.searchText);
+
+        vm.collapse = (vm.closed.length && vm.searchText.role) ?
+          { open: false, closed: false } :
+          { open: false, closed: true };
+      }, true);
     },
   ]);
