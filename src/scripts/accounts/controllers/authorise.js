36bdf8347fba2a855c28ae9ffcb0521ecebd407a
Yogesh Choudhary <yogesum@gmail.com>
2016-06-21 12:31:00 +0530

    feat(accounts): manage url redirect for internals from hire and partner

--- src/scripts/accounts/controllers/authorise.js
+++ src/scripts/accounts/controllers/authorise.js
@@ -4,16 +4,24 @@ angular.module('qui.accounts')
     '$location',
     '$window',
     'Session',
-    function AuthoriseController(OAuthorise, $location, $window, Session) {
+    'APP',
+    function AuthoriseController(OAuthorise, $location, $window, Session, APP) {
       const vm = this;
       const user = Session.read('userinfo');
       const params = $location.search();
+      const location = $window.location;
 
       const apps = {
         2: ['partnerquezx', 'partnerwordpress'],
         4: ['managequezx'],
         5: ['hirequezx'],
       };
+
+      // Handle redirect for internal user from hire and partner app
+      if (user.group_id === 4 && params.client_id !== 'managequezx') {
+        location.href = `${APP.manageServer}${params.state}`;
+        return;
+      }
 
       if (!~(apps[user.group_id] || []).indexOf(params.client_id)) {
         vm.error = 'Access Denied!';
@@ -29,7 +37,6 @@ angular.module('qui.accounts')
 
       vm.done = function done(allow) {
         const data = params;
-        const location = $window.location;
         data.allow = allow ? 'true' : 'false';
         OAuthorise
           .post(data)
