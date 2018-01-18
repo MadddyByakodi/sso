f72a3e937c3be1c95fc3ce495feb01a79051a500
Yogesh Choudhary <yogesum@gmail.com>
2018-01-18 19:47:27 +0530

    fix(captcha): removed dependency for now

--- client/app/oauth/client-signup/index.js
+++ client/app/oauth/client-signup/index.js
@@ -1,11 +1,10 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
-import vcRecaptcha from 'angular-recaptcha';
 import clientSignup from './client-signup.component';
 import routing from './client-signup.routes';
 
 export default angular
-  .module('oauth.client-signup', [uiRouter, vcRecaptcha])
+  .module('oauth.client-signup', [uiRouter])
   .config(routing)
   .component('clientSignup', clientSignup)
   .name;
