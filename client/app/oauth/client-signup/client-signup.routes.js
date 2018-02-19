5698c2421e3e599f134217f6ed4c13cbdc402b65
vishal011117 <33274141+vishal011117@users.noreply.github.com>
2018-02-19 19:06:51 +0530

    fix: change Url (#21)

--- client/app/oauth/client-signup/client-signup.routes.js
+++ client/app/oauth/client-signup/client-signup.routes.js
@@ -2,7 +2,7 @@
 export default function routes($stateProvider) {
   $stateProvider
     .state('oauth.client-signup', {
-      url: '/client-signup/{type:int}/:userId&&{src:int}',
+      url: '/client-signup/{type:int}/:userId?src',
       template: '<client-signup></client-signup>',
       data: { pageTitle: 'Client Signup' },
     });
