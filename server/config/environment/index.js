754d0c714431b78ff92ebbb9dba398c891c0e22f
MadddyByakodi <maddy.byakodi@gmail.com>
2019-01-28 11:50:18 +0530

    feat(google login): Sso login with google
    https://app.asana.com/0/974718724586871/1103150290510754/f

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -7,19 +7,27 @@ const root = path.normalize(`${__dirname}/../../..`);
 if (!fs.existsSync(path.join(root, '.env'))) {
   fs.writeFileSync(path.join(root, '.env'), fs.readFileSync(path.join(root, '.env.sample')));
 }
-
-const env = dotenv.config({ path: path.join(root, '.env') });
-const { PREFIX, DOMAIN } = env.parsed;
+const env = dotenv.config({ path: path.join(root, '.env') }).parsed;
+const IS_DEV = env.NODE_ENV === 'development';
+const { PREFIX, DOMAIN } = env;
 const config = {
   all: {
     env: process.env.NODE_ENV,
     // Server port
-    port: process.env.PORT || 4000,
+    port: process.env.PORT || 2000,
     ip: process.env.IP || '0.0.0.0',
     root,
-    URLS_ACCOUNTS: `${PREFIX}accounts.${DOMAIN}`,
+    URLS_SSO: `${PREFIX}sso.${DOMAIN}`,
     // Browser-sync port
-    browserSyncPort: process.env.BROWSER_SYNC_PORT || 3001,
+    browserSyncPort: process.env.BROWSER_SYNC_PORT || 2001,
+    auth: {
+      google: {
+        // scope: 'https://www.googleapis.com/auth/plus.me https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar',
+        redirect_uri: IS_DEV ? 'http://localhost:3001/signin' : `${env.PREFIX}accounts.${env.DOMAIN}/signin`,
+        client_id: env.GOOGLE_CLIENT_ID,
+        client_secret: env.GOOGLE_SECRET,
+      },
+    },
   },
   development: {
 
